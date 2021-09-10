5463301aad735a9197b1d417cb7dbee832df6073
Yevhen Shymotiuk <yevhenshymotiuk@pm.me>
2021-09-11 00:59:23 +0300

    Move vim config to nix-config repository

--- shell.nix
+++ shell.nix
@@ -1,39 +1,14 @@
 { pkgs }:
 
-with pkgs;
 let
-  myVim = vim_configurable.customize {
-    name = "vim";
-    vimrcConfig = {
-      packages.myVimPackage = with pkgs.vimPlugins; {
-        start = [ gruvbox vim-airline vim-airline-themes vim-nix  ];
-        opt = [ ];
-      };
-      customRC = ''
-        set number                    " Enable line numbers by default
-        set background=dark           " Set the default background to dark or light
-        set smartindent               " Automatically insert extra level of indentation
-        set tabstop=4                 " Default tabstop
-        set shiftwidth=4              " Default indent spacing
-        set expandtab                 " Expand [TABS] to spaces
-        syntax enable                 " Enable syntax highlighting
-        colorscheme gruvbox           " Set the default colour scheme
-        set t_Co=256                  " use 265 colors in vim
-        set spell spelllang=en_au     " Default spell checking language
-        hi clear SpellBad             " Clear any unwanted default settings
-        hi SpellBad cterm=underline   " Set the spell checking highlight style
-        hi SpellBad ctermbg=NONE      " Set the spell checking highlight background
-        match ErrorMsg '\s\+$'        "
-
-        let g:airline_powerline_fonts = 1   " Use powerline fonts
-        let g:airline_theme='gruvbox'       " Set the airline theme
-
-        set laststatus=2   " Set up the status line so it's coloured and always on
-      '';
-    };
+  config = builtins.fetchTarball {
+    url = "https://github.com/yevhenshymotiuk/nix-config/archive/master.tar.gz";
+    sha256 = "0bs8g7ihj75jn0biay9bcsmb1wsby8cmifhpwm41laxykc5qczp8";
   };
-in mkShell {
-  buildInputs = [ bat exa gdb ripgrep myVim ];
+  myVim = (import "${config}/home/programs/my-vim.nix" { inherit pkgs; }).pkg;
+in with pkgs;
+mkShell {
+  buildInputs = [ bat exa gdb myVim ripgrep ];
 
   shellHook = ''
     alias cat="bat --style plain"
