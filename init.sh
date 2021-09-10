140f1b2b882a585e0974274e648d4fb5a2654f4a
Yevhen Shymotiuk <yevhenshymotiuk@pm.me>
2021-09-10 15:09:38 +0300

    Source nix.sh if exists instead of installing nix

--- init.sh
+++ init.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
-# install nix
-if ! command -v nix &> /dev/null; then
+# install nix or source shell environment
+NIX_SH_FILE="$HOME/.nix-profile/etc/profile.d/nix.sh"
+if [[ ! -e "$NIX_SH_FILE" ]]; then
     sh <(curl -Ls https://nixos.org/nix/install)
-    source ~/.nix-profile/etc/profile.d/nix.sh
 fi
+source "$NIX_SH_FILE"
 
 # process substitution can not be used to start nix shell
 # because of https://github.com/NixOS/nix/issues/2734, so
