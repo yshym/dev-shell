2b68f3b5cb8cafd4e90ed7709d2a925be28648f6
Yevhen Shymotiuk <yevhenshymotiuk@pm.me>
2021-09-10 16:26:09 +0300

    Use Flakes to cache Nix evaluations

---
+++ flake.nix
@@ -0,0 +1,10 @@
+{
+  description = "Dev shell";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in { devShell = import ./shell.nix { inherit pkgs; }; });
+}
--- init.sh
+++ init.sh
@@ -2,20 +2,33 @@
 
 # install nix or source shell environment
 NIX_SH_FILE="$HOME/.nix-profile/etc/profile.d/nix.sh"
+NIX_EXISTS=1
 if [[ ! -e "$NIX_SH_FILE" ]]; then
+    NIX_EXISTS=0
     sh <(curl -Ls https://nixos.org/nix/install)
 fi
 source "$NIX_SH_FILE"
+! command nix flake --version &> /dev/null && nix-env -iA nixpkgs.nixFlakes
 
-# process substitution can not be used to start nix shell
-# because of https://github.com/NixOS/nix/issues/2734, so
-# temporary file should be created to store content of the
-# shell.nix file
-TEMP_SHELL_FILE="$(mktemp)"
-# download content of the shell file
-curl -Ls https://github.com/yevhenshymotiuk/dev-shell/raw/master/shell.nix \
-    > "$TEMP_SHELL_FILE"
-# start shell
-nix-shell "$TEMP_SHELL_FILE"
-# remove temporary shell file
-rm "$TEMP_SHELL_FILE"
+# explicitly enable flakes
+NIX_CONF_FILE="$HOME/.config/nix/nix.conf"
+if [[ ! -e "$NIX_CONF_FILE" ]]; then
+    mkdir -p "$HOME/.config/nix"
+    touch "$NIX_CONF_FILE"
+fi
+echo "experimental-features = nix-command flakes" > "$NIX_CONF_FILE"
+
+# start dev shell
+nix develop github:yevhenshymotiuk/dev-shell
+
+# remove nix if existed
+test $NIX_EXISTS -eq 0 \
+    && rm -rf \
+        /etc/profile/nix.sh \
+        /etc/nix \
+        /nix ~root/.nix-profile \
+        ~root/.nix-defexpr \
+        ~root/.nix-channels \
+        ~/.nix-profile \
+        ~/.nix-defexpr \
+        ~/.nix-channels \
