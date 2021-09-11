fc32c4db663f248d7603f69e53497347f6bea78d
Yevhen Shymotiuk <yevhenshymotiuk@pm.me>
2021-09-11 15:39:51 +0300

    Check for existance of flakes separately

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# install nix or source shell environment
+# install nix if not exists
 NIX_PROFILE_DIR="$HOME/.nix-profile"
 NIX_SH_FILE="$NIX_PROFILE_DIR/etc/profile.d/nix.sh"
 NIX_EXISTS=1
@@ -9,11 +9,12 @@ if [[ ! -e "$NIX_PROFILE_DIR" ]]; then
     sh <(curl -Ls https://nixos.org/nix/install)
 fi
 test -e "$NIX_SH_FILE" && source "$NIX_SH_FILE"
-! command nix flake --version &> /dev/null && nix-env -iA nixpkgs.nixFlakes
 
-if ! command cachix --version &> /dev/null; then
-    nix-env -iA nixpkgs.cachix
-    cachix use yevhenshymotiuk
+# replace stable version of nix with nix flakes if not exists
+NIX_FLAKES_EXISTS=1
+if ! command nix flake --version &> /dev/null; then
+    NIX_FLAKES_EXISTS=0
+    nix-env -iA nixpkgs.nixFlakes
 fi
 
 # explicitly enable flakes
@@ -24,10 +25,21 @@ if [[ ! -e "$NIX_CONF_FILE" ]]; then
 fi
 echo "experimental-features = nix-command flakes" > "$NIX_CONF_FILE"
 
+# install cachix if not exists
+if ! command cachix --version &> /dev/null; then
+    nix-env -iA nixpkgs.cachix
+    cachix use yevhenshymotiuk
+fi
+
 # start dev shell
 nix develop github:yevhenshymotiuk/dev-shell
 
-# remove nix if existed
+# replace flakes back if the stable version of nix was installed
+test $NIX_EXISTS -eq 1 && test $NIX_FLAKES_EXISTS -eq 0 \
+    && nix-env -iA nixpkgs.nix \
+    && sed -i "" -e "experimental-features = nix-command flakes" "$HOME/.config/nix"
+
+# remove nix if not existed
 test $NIX_EXISTS -eq 0 \
     && sudo rm -rf \
         /etc/profile/nix.sh \
